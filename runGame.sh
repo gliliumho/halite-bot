b6f9b40b121473ce91e9ee07a1ce9625b964754f
Kin Yee Ho <gliliumho@gmail.com>
2016-12-21 13:54:49 -0500

    Add lines to runGame.sh to clean logs & replays

--- runGame.sh
+++ runGame.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 if hash python3 2>/dev/null; then
-    ./halite -d "30 30" "python3 MyBot.py" "python3 DiscerningBot.py" "python3 OverkillBot.py" -t
+    ./halite -d "30 30" "python3 MyBot.py" "python3 OverkillBot.py" "node AliceBot.js" -t
 else
     ./halite -d "30 30" "python OverkillBot.py" "python DiscerningBot.py" "python BorderBot.py" -t
 fi
 
-if [ -e *.hlt ]; then mv *.hlt ./replays/; fi
+count=`ls -1 *.hlt 2>/dev/null | wc -l`
+if [ $count != 0 ]; then
+    mv *.hlt replays/
+fi
+
+count=`ls -1 *.log 2>/dev/null | wc -l`
+if [ $count != 0 ]; then
+    mv *.log logs/
+fi
